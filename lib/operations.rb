3ed93be27730b400e93475057cdb1500d909365b
Malary Robinson <robinmal16@gmail.com>
2019-04-11 01:38:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,12 @@
 def unsafe?(speed)
- 	if "#{speed}" == <40
-	 	return true
- 	elsif speed == >60
-	 	return true
- 	else
-	 	return false
-	end
+
 end
 
-unsafe?(79)
+
 
 def not_safe?(speed)
-
+	
 end
+	
+
+
